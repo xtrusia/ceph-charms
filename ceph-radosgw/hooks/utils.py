c12a1cdcaeb9d4882af5fc66cde0e69e40c2a430
James Page <james.page@canonical.com>
2014-01-24 16:02:57 +0000

    Rebase on charm-helpers

--- ceph-radosgw/hooks/utils.py
+++ ceph-radosgw/hooks/utils.py
@@ -7,95 +7,33 @@
 #  Paul Collins <paul.collins@canonical.com>
 #
 
-import os
-import subprocess
 import socket
-import sys
 import re
 
-
-def do_hooks(hooks):
-    hook = os.path.basename(sys.argv[0])
-
-    try:
-        hook_func = hooks[hook]
-    except KeyError:
-        juju_log('INFO',
-                 "This charm doesn't know how to handle '{}'.".format(hook))
-    else:
-        hook_func()
-
-
-def install(*pkgs):
-    cmd = [
-        'apt-get',
-        '-y',
-        'install'
-          ]
-    for pkg in pkgs:
-        cmd.append(pkg)
-    subprocess.check_call(cmd)
+from charmhelpers.core.hookenv import unit_get
+from charmhelpers.fetch import apt_install
 
 TEMPLATES_DIR = 'templates'
 
 try:
     import jinja2
 except ImportError:
-    install('python-jinja2')
+    apt_install('python-jinja2', fatal=True)
     import jinja2
 
 try:
     import dns.resolver
 except ImportError:
-    install('python-dnspython')
+    apt_install('python-dnspython', fatal=True)
     import dns.resolver
 
 
 def render_template(template_name, context, template_dir=TEMPLATES_DIR):
     templates = jinja2.Environment(
-                    loader=jinja2.FileSystemLoader(template_dir)
-                    )
+        loader=jinja2.FileSystemLoader(template_dir)
+    )
     template = templates.get_template(template_name)
     return template.render(context)
-
-
-CLOUD_ARCHIVE = \
-""" # Ubuntu Cloud Archive
-deb http://ubuntu-cloud.archive.canonical.com/ubuntu {} main
-"""
-
-
-def configure_source():
-    source = str(config_get('source'))
-    if not source:
-        return
-    if source.startswith('ppa:'):
-        cmd = [
-            'add-apt-repository',
-            source
-            ]
-        subprocess.check_call(cmd)
-    if source.startswith('cloud:'):
-        install('ubuntu-cloud-keyring')
-        pocket = source.split(':')[1]
-        with open('/etc/apt/sources.list.d/cloud-archive.list', 'w') as apt:
-            apt.write(CLOUD_ARCHIVE.format(pocket))
-    if source.startswith('http:'):
-        with open('/etc/apt/sources.list.d/quantum.list', 'w') as apt:
-            apt.write("deb " + source + "\n")
-        key = config_get('key')
-        if key:
-            cmd = [
-                'apt-key',
-                'adv', '--keyserver keyserver.ubuntu.com',
-                '--recv-keys', key
-                ]
-            subprocess.check_call(cmd)
-    cmd = [
-        'apt-get',
-        'update'
-        ]
-    subprocess.check_call(cmd)
 
 
 def enable_pocket(pocket):
@@ -108,103 +46,6 @@ def enable_pocket(pocket):
                 sources.write(re.sub('^# deb', 'deb', line))
             else:
                 sources.write(line)
-
-# Protocols
-TCP = 'TCP'
-UDP = 'UDP'
-
-
-def expose(port, protocol='TCP'):
-    cmd = [
-        'open-port',
-        '{}/{}'.format(port, protocol)
-        ]
-    subprocess.check_call(cmd)
-
-
-def juju_log(severity, message):
-    cmd = [
-        'juju-log',
-        '--log-level', severity,
-        message
-        ]
-    subprocess.check_call(cmd)
-
-
-def relation_ids(relation):
-    cmd = [
-        'relation-ids',
-        relation
-        ]
-    return subprocess.check_output(cmd).split()  # IGNORE:E1103
-
-
-def relation_list(rid):
-    cmd = [
-        'relation-list',
-        '-r', rid,
-        ]
-    return subprocess.check_output(cmd).split()  # IGNORE:E1103
-
-
-def relation_get(attribute, unit=None, rid=None):
-    cmd = [
-        'relation-get',
-        ]
-    if rid:
-        cmd.append('-r')
-        cmd.append(rid)
-    cmd.append(attribute)
-    if unit:
-        cmd.append(unit)
-    value = str(subprocess.check_output(cmd)).strip()
-    if value == "":
-        return None
-    else:
-        return value
-
-
-def relation_set(**kwargs):
-    cmd = [
-        'relation-set'
-        ]
-    args = []
-    for k, v in kwargs.items():
-        if k == 'rid' and v:
-            cmd.append('-r')
-            cmd.append(v)
-        elif k != 'rid':
-            args.append('{}={}'.format(k, v))
-    cmd += args
-    subprocess.check_call(cmd)
-
-
-def unit_get(attribute):
-    cmd = [
-        'unit-get',
-        attribute
-        ]
-    value = str(subprocess.check_output(cmd)).strip()
-    if value == "":
-        return None
-    else:
-        return value
-
-
-def config_get(attribute):
-    cmd = [
-        'config-get',
-        attribute
-        ]
-    value = str(subprocess.check_output(cmd)).strip()
-    if value == "":
-        return None
-    else:
-        return value
-
-
-def get_unit_hostname():
-    return socket.gethostname()
 
 
 def get_host_ip(hostname=unit_get('private-address')):
