5c6a1742bcad7519bbc58635870aaa67bd3bb3d4
Edward Hope-Morley <edward.hope-morley@canonical.com>
2015-03-26 09:03:52 -0700

    [trivial] fix amulet missing https_keystone setting error

--- ceph-radosgw/tests/basic_deployment.py
+++ ceph-radosgw/tests/basic_deployment.py
@@ -262,7 +262,6 @@ class CephRadosGwBasicDeployment(OpenStackAmuletDeployment):
             'auth_port': '35357',
             'auth_protocol': 'http',
             'private-address': u.valid_ip,
-            'https_keystone': 'False',
             'auth_host': u.valid_ip,
             'service_username': 'swift',
             'service_tenant_id': u.not_null,
