7394648cf128d9c56eed083eae663b72d095d7e6
James Page <james.page@ubuntu.com>
2015-10-06 21:15:38 +0100

    Add unit tests for service status

--- ceph-osd/Makefile
+++ ceph-osd/Makefile
@@ -3,8 +3,13 @@ PYTHON := /usr/bin/env python
 
 lint:
 	@flake8 --exclude hooks/charmhelpers,tests/charmhelpers \
-        hooks tests unit_tests
+		hooks tests unit_tests
 	@charm proof
+
+test:
+	@# Bundletester expects unit tests here.
+	@echo Starting unit tests...
+	@$(PYTHON) /usr/bin/nosetests --nologcapture --with-coverage  unit_tests
 
 functional_test:
 	@echo Starting Amulet tests...
@@ -13,7 +18,7 @@ functional_test:
 bin/charm_helpers_sync.py:
 	@mkdir -p bin
 	@bzr cat lp:charm-helpers/tools/charm_helpers_sync/charm_helpers_sync.py \
-        > bin/charm_helpers_sync.py
+		> bin/charm_helpers_sync.py
 
 sync: bin/charm_helpers_sync.py
 	$(PYTHON) bin/charm_helpers_sync.py -c charm-helpers-hooks.yaml
---
+++ ceph-osd/unit_tests/test_status.py
@@ -0,0 +1,56 @@
+import mock
+import test_utils
+
+with mock.patch('utils.get_unit_hostname'):
+    import ceph_hooks as hooks
+
+TO_PATCH = [
+    'status_set',
+    'config',
+    'ceph',
+    'relation_ids',
+    'relation_get',
+    'related_units',
+    'get_conf',
+]
+
+CEPH_MONS = [
+    'ceph/0',
+    'ceph/1',
+    'ceph/2',
+]
+
+class ServiceStatusTestCase(test_utils.CharmTestCase):
+
+    def setUp(self):
+        super(ServiceStatusTestCase, self).setUp(hooks, TO_PATCH)
+        self.config.side_effect = self.test_config.get
+
+    def test_assess_status_no_monitor_relation(self):
+        self.relation_ids.return_value = []
+        hooks.assess_status()
+        self.status_set.assert_called_with('blocked', mock.ANY)
+
+    def test_assess_status_monitor_relation_incomplete(self):
+        self.relation_ids.return_value = ['mon:1']
+        self.related_units.return_value = CEPH_MONS
+        self.get_conf.return_value = None
+        hooks.assess_status()
+        self.status_set.assert_called_with('waiting', mock.ANY)
+
+    def test_assess_status_monitor_complete_no_disks(self):
+        self.relation_ids.return_value = ['mon:1']
+        self.related_units.return_value = CEPH_MONS
+        self.get_conf.return_value = 'monitor-bootstrap-key'
+        self.ceph.get_running_osds.return_value = []
+        hooks.assess_status()
+        self.status_set.assert_called_with('blocked', mock.ANY)
+
+    def test_assess_status_monitor_complete_disks(self):
+        self.relation_ids.return_value = ['mon:1']
+        self.related_units.return_value = CEPH_MONS
+        self.get_conf.return_value = 'monitor-bootstrap-key'
+        self.ceph.get_running_osds.return_value = ['12345',
+                                                   '67890']
+        hooks.assess_status()
+        self.status_set.assert_called_with('active', mock.ANY)
