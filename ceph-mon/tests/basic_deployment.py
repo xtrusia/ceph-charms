cbc8e897e89fd4501296fe24061294063e9e2253
James Page <james.page@ubuntu.com>
2016-07-20 14:23:35 +0100

    Use bundletester for amulet test execution
    Switch to using bundletester for execution of functional tests,
    leveraging tox to build out test virtualenvs.
    
    Rename amulet tests inline with gate-* and dfs-* naming standards.
    
    Update README to refer to functional testing section of the charm
    guide.
    
    Change-Id: I0d378cdffdc2eaa5c34578e85aba6acb7a47a41d

--- ceph-mon/tests/basic_deployment.py
+++ ceph-mon/tests/basic_deployment.py
@@ -124,16 +124,16 @@ class CephBasicDeployment(OpenStackAmuletDeployment):
     def _initialize_tests(self):
         """Perform final initialization before tests get run."""
         # Access the sentries for inspecting service units
-        self.mysql_sentry = self.d.sentry.unit['mysql/0']
-        self.keystone_sentry = self.d.sentry.unit['keystone/0']
-        self.rabbitmq_sentry = self.d.sentry.unit['rabbitmq-server/0']
-        self.nova_sentry = self.d.sentry.unit['nova-compute/0']
-        self.glance_sentry = self.d.sentry.unit['glance/0']
-        self.cinder_sentry = self.d.sentry.unit['cinder/0']
-        self.ceph_osd_sentry = self.d.sentry.unit['ceph-osd/0']
-        self.ceph0_sentry = self.d.sentry.unit['ceph-mon/0']
-        self.ceph1_sentry = self.d.sentry.unit['ceph-mon/1']
-        self.ceph2_sentry = self.d.sentry.unit['ceph-mon/2']
+        self.mysql_sentry = self.d.sentry['mysql'][0]
+        self.keystone_sentry = self.d.sentry['keystone'][0]
+        self.rabbitmq_sentry = self.d.sentry['rabbitmq-server'][0]
+        self.nova_sentry = self.d.sentry['nova-compute'][0]
+        self.glance_sentry = self.d.sentry['glance'][0]
+        self.cinder_sentry = self.d.sentry['cinder'][0]
+        self.ceph_osd_sentry = self.d.sentry['ceph-osd'][0]
+        self.ceph0_sentry = self.d.sentry['ceph-mon'][0]
+        self.ceph1_sentry = self.d.sentry['ceph-mon'][1]
+        self.ceph2_sentry = self.d.sentry['ceph-mon'][2]
         u.log.debug('openstack release val: {}'.format(
             self._get_openstack_release()))
         u.log.debug('openstack release str: {}'.format(
