6adc932f9018adfffc807501a6f154b8834484f0
Chris Holcombe <xfactor973@gmail.com>
2016-09-06 15:31:04 -0700

    Move upgrade code to shared lib
    Moving the ceph mon upgrade code over to the
    ceph shared library. This will make it easier
    to make patches and have them be applied to all
    3 charms at once.
    
    Change-Id: I541269d05e6ff8883233a21c78ebe9df89b9e797

--- ceph-osd/hooks/ceph_hooks.py
+++ ceph-osd/hooks/ceph_hooks.py
@@ -15,12 +15,9 @@
 # limitations under the License.
 import os
 import shutil
-import random
-import subprocess
 import sys
 import tempfile
 import socket
-import time
 import netifaces
 
 sys.path.append('lib')
@@ -45,10 +42,7 @@ from charmhelpers.core.hookenv import (
 from charmhelpers.core.host import (
     umount,
     mkdir,
-    cmp_pkgrevno,
-    service_stop,
-    service_start,
-    chownr)
+    cmp_pkgrevno)
 from charmhelpers.fetch import (
     add_source,
     apt_install,
@@ -75,10 +69,7 @@ from charmhelpers.contrib.network.ip import (
     format_ipv6_addr,
 )
 from charmhelpers.contrib.storage.linux.ceph import (
-    CephConfContext,
-    monitor_key_set,
-    monitor_key_exists,
-    monitor_key_get)
+    CephConfContext)
 from charmhelpers.contrib.charmsupport import nrpe
 from charmhelpers.contrib.hardening.harden import harden
 
@@ -120,171 +111,14 @@ def check_for_upgrade():
         if new_version == upgrade_paths[old_version]:
             log("{} to {} is a valid upgrade path.  Proceeding.".format(
                 old_version, new_version))
-            roll_osd_cluster(new_version)
+            ceph.roll_osd_cluster(new_version=new_version,
+                                  upgrade_key='osd-upgrade')
         else:
             # Log a helpful error message
             log("Invalid upgrade path from {} to {}.  "
                 "Valid paths are: {}".format(old_version,
                                              new_version,
                                              pretty_print_upgrade_paths()))
-
-
-def lock_and_roll(my_name, version):
-    start_timestamp = time.time()
-
-    log('monitor_key_set {}_start {}'.format(
-        my_name,
-        version,
-        start_timestamp))
-    monitor_key_set('osd-upgrade', "{}_{}_start".format(my_name, version),
-                    start_timestamp)
-    log("Rolling")
-    # This should be quick
-    upgrade_osd()
-    log("Done")
-
-    stop_timestamp = time.time()
-    # Set a key to inform others I am finished
-    log('monitor_key_set {}_{}_done {}'.format(my_name,
-                                               version,
-                                               stop_timestamp))
-    monitor_key_set('osd-upgrade', "{}_{}_done".format(my_name, version),
-                    stop_timestamp)
-
-
-def wait_on_previous_node(previous_node, version):
-    log("Previous node is: {}".format(previous_node))
-
-    previous_node_finished = monitor_key_exists(
-        'osd-upgrade',
-        "{}_{}_done".format(previous_node, version))
-
-    while previous_node_finished is False:
-        log("{} is not finished. Waiting".format(previous_node))
-        # Has this node been trying to upgrade for longer than
-        # 10 minutes?
-        # If so then move on and consider that node dead.
-
-        # NOTE: This assumes the clusters clocks are somewhat accurate
-        # If the hosts clock is really far off it may cause it to skip
-        # the previous node even though it shouldn't.
-        current_timestamp = time.time()
-        previous_node_start_time = monitor_key_get(
-            'osd-upgrade',
-            "{}_{}_start".format(previous_node, version))
-        if (current_timestamp - (10 * 60)) > previous_node_start_time:
-            # Previous node is probably dead.  Lets move on
-            if previous_node_start_time is not None:
-                log(
-                    "Waited 10 mins on node {}. current time: {} > "
-                    "previous node start time: {} Moving on".format(
-                        previous_node,
-                        (current_timestamp - (10 * 60)),
-                        previous_node_start_time))
-                return
-        else:
-            # I have to wait.  Sleep a random amount of time and then
-            # check if I can lock,upgrade and roll.
-            wait_time = random.randrange(5, 30)
-            log('waiting for {} seconds'.format(wait_time))
-            time.sleep(wait_time)
-            previous_node_finished = monitor_key_exists(
-                'osd-upgrade',
-                "{}_{}_done".format(previous_node, version))
-
-
-def get_upgrade_position(osd_sorted_list, match_name):
-    for index, item in enumerate(osd_sorted_list):
-        if item.name == match_name:
-            return index
-    return None
-
-
-# Edge cases:
-# 1. Previous node dies on upgrade, can we retry?
-# 2. This assumes that the osd failure domain is not set to osd.
-#    It rolls an entire server at a time.
-def roll_osd_cluster(new_version):
-    """
-    This is tricky to get right so here's what we're going to do.
-    There's 2 possible cases: Either I'm first in line or not.
-    If I'm not first in line I'll wait a random time between 5-30 seconds
-    and test to see if the previous osd is upgraded yet.
-
-    TODO: If you're not in the same failure domain it's safe to upgrade
-     1. Examine all pools and adopt the most strict failure domain policy
-        Example: Pool 1: Failure domain = rack
-        Pool 2: Failure domain = host
-        Pool 3: Failure domain = row
-
-        outcome: Failure domain = host
-    """
-    log('roll_osd_cluster called with {}'.format(new_version))
-    my_name = socket.gethostname()
-    osd_tree = ceph.get_osd_tree(service='osd-upgrade')
-    # A sorted list of osd unit names
-    osd_sorted_list = sorted(osd_tree)
-    log("osd_sorted_list: {}".format(osd_sorted_list))
-
-    try:
-        position = get_upgrade_position(osd_sorted_list, my_name)
-        log("upgrade position: {}".format(position))
-        if position == 0:
-            # I'm first!  Roll
-            # First set a key to inform others I'm about to roll
-            lock_and_roll(my_name=my_name, version=new_version)
-        else:
-            # Check if the previous node has finished
-            status_set('blocked',
-                       'Waiting on {} to finish upgrading'.format(
-                           osd_sorted_list[position - 1].name))
-            wait_on_previous_node(
-                previous_node=osd_sorted_list[position - 1].name,
-                version=new_version)
-            lock_and_roll(my_name=my_name, version=new_version)
-    except ValueError:
-        log("Failed to find name {} in list {}".format(
-            my_name, osd_sorted_list))
-        status_set('blocked', 'failed to upgrade osd')
-
-
-def upgrade_osd():
-    current_version = ceph.get_version()
-    status_set("maintenance", "Upgrading osd")
-    log("Current ceph version is {}".format(current_version))
-    new_version = config('release-version')
-    log("Upgrading to: {}".format(new_version))
-
-    try:
-        add_source(config('source'), config('key'))
-        apt_update(fatal=True)
-    except subprocess.CalledProcessError as err:
-        log("Adding the ceph source failed with message: {}".format(
-            err.message))
-        status_set("blocked", "Upgrade to {} failed".format(new_version))
-        sys.exit(1)
-    try:
-        if ceph.systemd():
-            for osd_id in ceph.get_local_osd_ids():
-                service_stop('ceph-osd@{}'.format(osd_id))
-        else:
-            service_stop('ceph-osd-all')
-        apt_install(packages=ceph.PACKAGES, fatal=True)
-
-        # Ensure the ownership of Ceph's directories is correct
-        chownr(path=os.path.join(os.sep, "var", "lib", "ceph"),
-               owner=ceph.ceph_user(),
-               group=ceph.ceph_user())
-        if ceph.systemd():
-            for osd_id in ceph.get_local_osd_ids():
-                service_start('ceph-osd@{}'.format(osd_id))
-        else:
-            service_start('ceph-osd-all')
-    except subprocess.CalledProcessError as err:
-        log("Stopping ceph and upgrading packages failed "
-            "with message: {}".format(err.message))
-        status_set("blocked", "Upgrade to {} failed".format(new_version))
-        sys.exit(1)
 
 
 def tune_network_adapters():
