67144644e1f0124843120906249594d7e5d229e8
Billy Olsen <billy.olsen@gmail.com>
2017-03-13 16:54:18 -0700

    Upgrade OSDs one at a time when changing ownership
    Some upgrade scenarios (hammer->jewel) require that the ownership
    of the ceph osd directories are changed from root:root to ceph:ceph.
    This patch improves the upgrade experience by upgrading one OSD at
    a time as opposed to stopping all services, changing file ownership,
    and then restarting all services at once.
    
    This patch makes use of the `setuser match path` directive in the
    ceph.conf, which causes the ceph daemon to start as the owner of the
    OSD's root directory. This allows the ceph OSDs to continue running
    should an unforeseen incident occur as part of this upgrade.
    
    Change-Id: I00fdbe0fd113c56209429341f0a10797e5baee5a
    Closes-Bug: #1662591

--- ceph-osd/unit_tests/test_upgrade.py
+++ ceph-osd/unit_tests/test_upgrade.py
@@ -2,7 +2,7 @@ import unittest
 
 __author__ = 'Chris Holcombe <chris.holcombe@canonical.com>'
 
-from mock import patch, MagicMock
+from mock import call, patch, MagicMock
 
 from ceph_hooks import check_for_upgrade
 
@@ -18,13 +18,17 @@ def config_side_effect(*args):
 
 class UpgradeRollingTestCase(unittest.TestCase):
 
+    @patch('ceph_hooks.ceph.dirs_need_ownership_update')
     @patch('ceph_hooks.ceph.is_bootstrapped')
     @patch('ceph_hooks.ceph.resolve_ceph_version')
+    @patch('ceph_hooks.emit_cephconf')
     @patch('ceph_hooks.hookenv')
     @patch('ceph_hooks.host')
     @patch('ceph_hooks.ceph.roll_osd_cluster')
     def test_check_for_upgrade(self, roll_osd_cluster, host, hookenv,
-                               version, is_bootstrapped):
+                               emit_cephconf, version, is_bootstrapped,
+                               dirs_need_ownership_update):
+        dirs_need_ownership_update.return_value = False
         is_bootstrapped.return_value = True
         version.side_effect = ['firefly', 'hammer']
         host.lsb_release.return_value = {
@@ -36,9 +40,33 @@ class UpgradeRollingTestCase(unittest.TestCase):
                                       config_side_effect('source')]
         check_for_upgrade()
 
-        roll_osd_cluster.assert_called_with(
-            new_version='hammer',
-            upgrade_key='osd-upgrade')
+        roll_osd_cluster.assert_called_with(new_version='hammer',
+                                            upgrade_key='osd-upgrade')
+        emit_cephconf.assert_has_calls([call(upgrading=True),
+                                        call(upgrading=False)])
+
+    @patch('ceph_hooks.ceph.dirs_need_ownership_update')
+    @patch('ceph_hooks.ceph.is_bootstrapped')
+    @patch('ceph_hooks.ceph.resolve_ceph_version')
+    @patch('ceph_hooks.emit_cephconf')
+    @patch('ceph_hooks.hookenv')
+    @patch('ceph_hooks.host.lsb_release')
+    @patch('ceph_hooks.ceph.roll_osd_cluster')
+    def test_resume_failed_upgrade(self, roll_osd_cluster, lsb_release,
+                                   hookenv, emit_cephconf, version,
+                                   is_bootstrapped,
+                                   dirs_need_ownership_update):
+        dirs_need_ownership_update.return_value = True
+        is_bootstrapped.return_value = True
+        version.side_effect = ['jewel', 'jewel']
+        lsb_release.return_value = {'DISTRIB_CODENAME': 'trusty'}
+
+        check_for_upgrade()
+
+        roll_osd_cluster.assert_called_with(new_version='jewel',
+                                            upgrade_key='osd-upgrade')
+        emit_cephconf.assert_has_calls([call(upgrading=True),
+                                        call(upgrading=False)])
 
     @patch('ceph_hooks.ceph.is_bootstrapped')
     @patch('ceph_hooks.ceph.resolve_ceph_version')
