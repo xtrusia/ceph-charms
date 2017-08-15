e557935219119eff8efcd1c630e2d76afe607fc4
David Ames <david.ames@canonical.com>
2017-08-15 10:11:30 -0700

    Dual Stack VIPs
    Enable dual stack IPv4 and IPv6 VIPs on the same interface.
    HAProxy always listens on both IPv4 and IPv6 allowing connectivity
    on either protocol.
    
    charm-helpers sync for HAProxy template changes.
    
    Change-Id: Ibc95f322df29857df1c16f1ac1ebe04b5a2bc748

--- ceph-radosgw/hooks/hooks.py
+++ ceph-radosgw/hooks/hooks.py
@@ -29,6 +29,7 @@ from charmhelpers.core.hookenv import (
     relation_set,
     log,
     DEBUG,
+    WARNING,
     Hooks, UnregisteredHookError,
     status_set,
 )
@@ -287,6 +288,14 @@ def ha_relation_joined(relation_id=None):
 
             if iface is not None:
                 vip_key = 'res_cephrg_{}_vip'.format(iface)
+                if vip_key in vip_group:
+                    if vip not in resource_params[vip_key]:
+                        vip_key = '{}_{}'.format(vip_key, vip_params)
+                    else:
+                        log("Resource '%s' (vip='%s') already exists in "
+                            "vip group - skipping" % (vip_key, vip), WARNING)
+                        continue
+
                 resources[vip_key] = res_rgw_vip
                 resource_params[vip_key] = (
                     'params {ip}="{vip}" cidr_netmask="{netmask}"'
