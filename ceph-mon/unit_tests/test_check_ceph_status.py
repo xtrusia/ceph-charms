dba22cbf19588da006b5efe8113da17fb3efff8b
Xav Paice <xav.paice@canonical.com>
2018-04-18 19:19:50 +1200

    Update Nagios check for Luminous
    This adds a test to see if the ceph status output looks like Luminous or
    newer, and if so changes the output used to collect info.
    
    Change-Id: I98d194c329aace3c412701e06632dbfedfadefc7
    Closes-Bug: #1756864

--- ceph-mon/unit_tests/test_check_ceph_status.py
+++ ceph-mon/unit_tests/test_check_ceph_status.py
@@ -26,7 +26,15 @@ import check_ceph_status
 @patch('subprocess.check_output')
 class NagiosTestCase(unittest.TestCase):
 
-    def test_health_ok(self, mock_subprocess):
+    def test_get_ceph_version(self, mock_subprocess):
+        mock_subprocess.return_value = 'ceph version 10.2.9 ' \
+            '(2ee413f77150c0f375ff6f10edd6c8f9c7d060d0)'.encode('UTF-8')
+        ceph_version = check_ceph_status.get_ceph_version()
+        self.assertEqual(ceph_version, [10, 2, 9])
+
+    @patch('check_ceph_status.get_ceph_version')
+    def test_health_ok(self, mock_ceph_version, mock_subprocess):
+        mock_ceph_version.return_value = [10, 2, 9]
         with open('unit_tests/ceph_ok.json') as f:
             tree = f.read()
         mock_subprocess.return_value = tree.encode('UTF-8')
@@ -34,7 +42,19 @@ class NagiosTestCase(unittest.TestCase):
         check_output = check_ceph_status.check_ceph_status(args)
         self.assertRegex(check_output, r"^All OK$")
 
-    def test_health_warn(self, mock_subprocess):
+    @patch('check_ceph_status.get_ceph_version')
+    def test_health_ok_luminous(self, mock_ceph_version, mock_subprocess):
+        mock_ceph_version.return_value = [12, 2, 0]
+        with open('unit_tests/ceph_ok_luminous.json') as f:
+            tree = f.read()
+        mock_subprocess.return_value = tree.encode('UTF-8')
+        args = check_ceph_status.parse_args(['--degraded_thresh', '1'])
+        check_output = check_ceph_status.check_ceph_status(args)
+        self.assertRegex(check_output, r"^All OK$")
+
+    @patch('check_ceph_status.get_ceph_version')
+    def test_health_warn(self, mock_ceph_version, mock_subprocess):
+        mock_ceph_version.return_value = [10, 2, 9]
         with open('unit_tests/ceph_warn.json') as f:
             tree = f.read()
         mock_subprocess.return_value = tree.encode('UTF-8')
@@ -42,7 +62,9 @@ class NagiosTestCase(unittest.TestCase):
         self.assertRaises(check_ceph_status.WarnError,
                           lambda: check_ceph_status.check_ceph_status(args))
 
-    def test_health_crit(self, mock_subprocess):
+    @patch('check_ceph_status.get_ceph_version')
+    def test_health_crit(self, mock_ceph_version, mock_subprocess):
+        mock_ceph_version.return_value = [10, 2, 9]
         with open('unit_tests/ceph_crit.json') as f:
             tree = f.read()
         mock_subprocess.return_value = tree.encode('UTF-8')
@@ -50,7 +72,19 @@ class NagiosTestCase(unittest.TestCase):
         self.assertRaises(check_ceph_status.CriticalError,
                           lambda: check_ceph_status.check_ceph_status(args))
 
-    def test_health_lotsdegraded(self, mock_subprocess):
+    @patch('check_ceph_status.get_ceph_version')
+    def test_health_crit_luminous(self, mock_ceph_version, mock_subprocess):
+        mock_ceph_version.return_value = [12, 2, 0]
+        with open('unit_tests/ceph_crit_luminous.json') as f:
+            tree = f.read()
+        mock_subprocess.return_value = tree.encode('UTF-8')
+        args = check_ceph_status.parse_args(['--degraded_thresh', '1'])
+        self.assertRaises(check_ceph_status.CriticalError,
+                          lambda: check_ceph_status.check_ceph_status(args))
+
+    @patch('check_ceph_status.get_ceph_version')
+    def test_health_lotsdegraded(self, mock_ceph_version, mock_subprocess):
+        mock_ceph_version.return_value = [10, 2, 9]
         with open('unit_tests/ceph_params.json') as f:
             tree = f.read()
         mock_subprocess.return_value = tree.encode('UTF-8')
@@ -58,7 +92,9 @@ class NagiosTestCase(unittest.TestCase):
         self.assertRaises(check_ceph_status.CriticalError,
                           lambda: check_ceph_status.check_ceph_status(args))
 
-    def test_health_nodeepscrub(self, mock_subprocess):
+    @patch('check_ceph_status.get_ceph_version')
+    def test_health_nodeepscrub(self, mock_ceph_version, mock_subprocess):
+        mock_ceph_version.return_value = [10, 2, 9]
         with open('unit_tests/ceph_nodeepscrub.json') as f:
             tree = f.read()
         mock_subprocess.return_value = tree.encode('UTF-8')
@@ -66,7 +102,9 @@ class NagiosTestCase(unittest.TestCase):
         self.assertRaises(check_ceph_status.CriticalError,
                           lambda: check_ceph_status.check_ceph_status(args))
 
-    def test_health_nodeepscrubok(self, mock_subprocess):
+    @patch('check_ceph_status.get_ceph_version')
+    def test_health_nodeepscrubok(self, mock_ceph_version, mock_subprocess):
+        mock_ceph_version.return_value = [10, 2, 9]
         with open('unit_tests/ceph_nodeepscrub.json') as f:
             tree = f.read()
         mock_subprocess.return_value = tree.encode('UTF-8')
