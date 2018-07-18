28b5a99a8b17cbe02de97ce856fa885dd92706fb
Chris MacNaughton <chris.macnaughton@canonical.com>
2018-07-18 10:20:23 +0200

    Remove old nova-compute / mysql relation
    Change-Id: I45bf4f6b9eb1fb50629acc0f00ccde774a7a5a03
    Closes-Bug: #1713807

--- ceph-radosgw/tests/basic_deployment.py
+++ ceph-radosgw/tests/basic_deployment.py
@@ -78,7 +78,6 @@ class CephRadosGwBasicDeployment(OpenStackAmuletDeployment):
     def _add_relations(self):
         """Add all of the relations for the services."""
         relations = {
-            'nova-compute:shared-db': 'percona-cluster:shared-db',
             'nova-compute:amqp': 'rabbitmq-server:amqp',
             'nova-compute:image-service': 'glance:image-service',
             'nova-compute:ceph': 'ceph-mon:client',
