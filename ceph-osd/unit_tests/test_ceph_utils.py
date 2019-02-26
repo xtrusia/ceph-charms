7cf07ff73e5c75b3e93c2c48c562a849734059ea
Andre Ruiz <andre.ruiz@canonical.com>
2019-02-26 17:13:40 +0100

    Implement new option to enable discard on SSDs
    This change implements a new option called 'bdev-enable-discard' to control
    behaviour of issuing discards to SSDs on ceph bluestore. The new code tries
    to autodetect cases where it should be enabled by default but will allow
    forcing if desired.
    
    Change-Id: I7b83605c827eb4058bc4b46c92eb114c11108c93
    Closes-Bug: #1788433

--- ceph-osd/unit_tests/test_ceph_utils.py
+++ ceph-osd/unit_tests/test_ceph_utils.py
@@ -61,3 +61,57 @@ class CephUtilsTestCase(unittest.TestCase):
         mock_os_path_exists.assert_called()
         mock_get_blacklist.assert_called()
         self.assertEqual(devices, set(['/dev/vdb']))
+
+    @patch('os.path.exists')
+    @patch.object(utils, 'is_sata30orless')
+    def test_should_enable_discard_yes(self, mock_is_sata30orless,
+                                       mock_os_path_exists):
+        devices = ['/dev/sda', '/dev/vda', '/dev/nvme0n1']
+        mock_os_path_exists.return_value = True
+        mock_is_sata30orless.return_value = False
+        ret = utils.should_enable_discard(devices)
+        mock_os_path_exists.assert_called()
+        mock_is_sata30orless.assert_called()
+        self.assertEqual(ret, True)
+
+    @patch('os.path.exists')
+    @patch.object(utils, 'is_sata30orless')
+    def test_should_enable_discard_no(self, mock_is_sata30orless,
+                                      mock_os_path_exists):
+        devices = ['/dev/sda', '/dev/vda', '/dev/nvme0n1']
+        mock_os_path_exists.return_value = True
+        mock_is_sata30orless.return_value = True
+        ret = utils.should_enable_discard(devices)
+        mock_os_path_exists.assert_called()
+        mock_is_sata30orless.assert_called()
+        self.assertEqual(ret, False)
+
+    @patch('subprocess.check_output')
+    def test_is_sata30orless_sata31(self, mock_subprocess_check_output):
+        extcmd_output = (b'supressed text\nSATA Version is:  '
+                         b'SATA 3.1, 6.0 Gb/s (current: 6.0 Gb/s)\n'
+                         b'supressed text\n\n')
+        mock_subprocess_check_output.return_value = extcmd_output
+        ret = utils.is_sata30orless('/dev/sda')
+        mock_subprocess_check_output.assert_called()
+        self.assertEqual(ret, False)
+
+    @patch('subprocess.check_output')
+    def test_is_sata30orless_sata30(self, mock_subprocess_check_output):
+        extcmd_output = (b'supressed text\nSATA Version is:  '
+                         b'SATA 3.0, 6.0 Gb/s (current: 6.0 Gb/s)\n'
+                         b'supressed text\n\n')
+        mock_subprocess_check_output.return_value = extcmd_output
+        ret = utils.is_sata30orless('/dev/sda')
+        mock_subprocess_check_output.assert_called()
+        self.assertEqual(ret, True)
+
+    @patch('subprocess.check_output')
+    def test_is_sata30orless_sata26(self, mock_subprocess_check_output):
+        extcmd_output = (b'supressed text\nSATA Version is:  '
+                         b'SATA 2.6, 3.0 Gb/s (current: 3.0 Gb/s)\n'
+                         b'supressed text\n\n')
+        mock_subprocess_check_output.return_value = extcmd_output
+        ret = utils.is_sata30orless('/dev/sda')
+        mock_subprocess_check_output.assert_called()
+        self.assertEqual(ret, True)
