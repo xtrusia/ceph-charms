1004cbca2e70e717e6d7ee4fa7a60f959f81bd7f
Robert Gildein <robert.gildein@canonical.com>
2020-10-15 17:51:21 +0200

    add function to check if device exists
    - if the device does not exists, the action failed with message:
      '/dev/<device>: Device does not exists.'
    
    Closes-Bug: #1885336
    Change-Id: I417c0074fe64c2afceecdfb09b8673930087f65f

--- ceph-osd/unit_tests/test_actions_zap_disk.py
+++ ceph-osd/unit_tests/test_actions_zap_disk.py
@@ -35,6 +35,7 @@ class ZapDiskActionTests(CharmTestCase):
         self.kv.return_value = self.kv
         self.hookenv.local_unit.return_value = "ceph-osd-test/0"
 
+    @mock.patch('os.path.exists', mock.MagicMock(return_value=True))
     @mock.patch.object(zap_disk, 'zap_disk')
     def test_authorized_zap_single_disk(self,
                                         _zap_disk):
@@ -44,6 +45,7 @@ class ZapDiskActionTests(CharmTestCase):
                 'devices': '/dev/vdb',
                 'i-really-mean-it': True,
             }.get(arg)
+
         self.hookenv.action_get.side_effect = side_effect
         self.kv.get.return_value = ['/dev/vdb', '/dev/vdz']
         zap_disk.zap()
@@ -57,6 +59,7 @@ class ZapDiskActionTests(CharmTestCase):
                        "osd-devices=\"/dev/vdb\""
         })
 
+    @mock.patch('os.path.exists', mock.MagicMock(return_value=True))
     @mock.patch.object(zap_disk, 'zap_disk')
     def test_authorized_zap_multiple_disks(self,
                                            _zap_disk):
@@ -66,6 +69,7 @@ class ZapDiskActionTests(CharmTestCase):
                 'devices': '/dev/vdb /dev/vdc',
                 'i-really-mean-it': True,
             }.get(arg)
+
         self.hookenv.action_get.side_effect = side_effect
         self.kv.get.return_value = ['/dev/vdb', '/dev/vdz']
         zap_disk.zap()
@@ -82,15 +86,17 @@ class ZapDiskActionTests(CharmTestCase):
                        "osd-devices=\"/dev/vdb /dev/vdc\""
         })
 
+    @mock.patch('os.path.exists', mock.MagicMock(return_value=True))
     @mock.patch.object(zap_disk, 'zap_disk')
     def test_wont_zap_non_block_device(self,
-                                       _zap_disk,):
+                                       _zap_disk):
         """Will not zap a disk that isn't a block device"""
         def side_effect(arg):
             return {
                 'devices': '/dev/vdb',
                 'i-really-mean-it': True,
             }.get(arg)
+
         self.hookenv.action_get.side_effect = side_effect
         self.is_block_device.return_value = False
         zap_disk.zap()
@@ -98,6 +104,7 @@ class ZapDiskActionTests(CharmTestCase):
         self.hookenv.action_fail.assert_called_with(
             "1 devices are not block devices: /dev/vdb")
 
+    @mock.patch('os.path.exists', mock.MagicMock(return_value=True))
     @mock.patch.object(zap_disk, 'zap_disk')
     def test_wont_zap_mounted_block_device(self,
                                            _zap_disk):
@@ -107,6 +114,7 @@ class ZapDiskActionTests(CharmTestCase):
                 'devices': '/dev/vdb',
                 'i-really-mean-it': True,
             }.get(arg)
+
         self.hookenv.action_get.side_effect = side_effect
         self.is_device_mounted.return_value = True
         zap_disk.zap()
@@ -114,6 +122,7 @@ class ZapDiskActionTests(CharmTestCase):
         self.hookenv.action_fail.assert_called_with(
             "1 devices are mounted: /dev/vdb")
 
+    @mock.patch('os.path.exists', mock.MagicMock(return_value=True))
     @mock.patch.object(zap_disk, 'zap_disk')
     def test_wont_zap__mounted_bluestore_device(self,
                                                 _zap_disk):
@@ -123,6 +132,7 @@ class ZapDiskActionTests(CharmTestCase):
                 'devices': '/dev/vdb',
                 'i-really-mean-it': True,
             }.get(arg)
+
         self.hookenv.action_get.side_effect = side_effect
         self.is_active_bluestore_device.return_value = True
         zap_disk.zap()
@@ -130,6 +140,7 @@ class ZapDiskActionTests(CharmTestCase):
         self.hookenv.action_fail.assert_called_with(
             "1 devices are mounted: /dev/vdb")
 
+    @mock.patch('os.path.exists', mock.MagicMock(return_value=True))
     @mock.patch.object(zap_disk, 'zap_disk')
     def test_wont_zap__mapped_luks_device(self, _zap_disk):
         """Will not zap a disk that has a LUKS header"""
@@ -138,6 +149,7 @@ class ZapDiskActionTests(CharmTestCase):
                 'devices': '/dev/vdb',
                 'i-really-mean-it': True,
             }.get(arg)
+
         self.hookenv.action_get.side_effect = side_effect
         self.is_active_bluestore_device.return_value = False
         self.is_mapped_luks_device.return_value = True
@@ -146,6 +158,7 @@ class ZapDiskActionTests(CharmTestCase):
         self.hookenv.action_fail.assert_called_with(
             "1 devices are mounted: /dev/vdb")
 
+    @mock.patch('os.path.exists', mock.MagicMock(return_value=True))
     @mock.patch.object(zap_disk, 'zap_disk')
     def test_zap_luks_not_mapped(self, _zap_disk):
         """Will zap disk with extra config set"""
@@ -170,3 +183,35 @@ class ZapDiskActionTests(CharmTestCase):
                        "run-action ceph-osd-test/0 add-disk "
                        "osd-devices=\"/dev/vdb\""
         })
+
+    @mock.patch.object(zap_disk, 'zap_disk')
+    def test_wont_zap_non_existent_device(self, _zap_disk):
+        """Won't zap non-existent disk"""
+        def side_effect(arg):
+            return {
+                'devices': '/dev/not-valid-disk',
+                'i-really-mean-it': True,
+            }.get(arg)
+
+        self.hookenv.action_get.side_effect = side_effect
+        zap_disk.zap()
+        _zap_disk.assert_not_called()
+        self.hookenv.action_fail.assert_called_with(
+            'Failed due to: /dev/not-valid-disk: Device does not exist.')
+        self.hookenv.action_set.assert_not_called()
+
+    @mock.patch.object(zap_disk, 'zap_disk')
+    def test_wont_zap_not_abs_path(self, _zap_disk):
+        """Won't zap not absolute path"""
+        def side_effect(arg):
+            return {
+                'devices': 'not-absolute',
+                'i-really-mean-it': True,
+            }.get(arg)
+
+        self.hookenv.action_get.side_effect = side_effect
+        zap_disk.zap()
+        _zap_disk.assert_not_called()
+        self.hookenv.action_fail.assert_called_with(
+            'Failed due to: not-absolute: Not absolute path.')
+        self.hookenv.action_set.assert_not_called()
