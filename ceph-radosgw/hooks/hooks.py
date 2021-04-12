xtrusia/ceph-charms

commit 798ca49b2f836fd8e0808fc73de039e073a8e91a

Close previously opened ports on port config change
When the charm config option `port` is changed,
the previously opened port is not closed.

This leads to leaks of open ports (potential security
issue), and long ports field on status after tests:

Test:

    $ juju config ceph-radosgw port=1111
    $ juju config ceph-radosgw port=2222
    $ juju config ceph-radosgw port=3333

    $ juju status ceph-radosgw
    ...
    Unit Workload Agent Machine Public address Ports Message
    ceph-radosgw/1* blocked idle 3 10.5.2.210
    80/tcp,1111/tcp,2222/tcp,3333/tcp Missing relations: mon
    ...

    $ juju run --unit ceph-radosgw/1 'opened-ports'
    80/tcp
    1111/tcp
    2222/tcp
    3333/tcp

Patched:

    $ juju run --unit ceph-radosgw/1 'opened-ports'
    80/tcp
    1111/tcp
    1234/tcp
    2222/tcp
    3333/tcp
    33331/tcp
    33332/tcp
    33334/tcp

    $ juju config ceph-radosgw port=33335

    $ juju run --unit ceph-radosgw/1 'opened-ports'
    33335/tcp

    $ juju status ceph-radosgw
    ...
    Unit             Workload  Agent  Machine  Public address  Ports
    Message
    ceph-radosgw/1*  blocked   idle   3        10.5.2.210      33335/tcp
    Missing relations: mon

    @ unit log
    2021-03-24 13:20:51 INFO juju-log Closed port 80 in favor of port 33335
    2021-03-24 13:20:51 INFO juju-log Closed port 1111 in favor of port 33335
    2021-03-24 13:20:51 INFO juju-log Closed port 1234 in favor of port 33335
    2021-03-24 13:20:51 INFO juju-log Closed port 2222 in favor of port 33335
    2021-03-24 13:20:52 INFO juju-log Closed port 3333 in favor of port 33335
    2021-03-24 13:20:52 INFO juju-log Closed port 33331 in favor of port 33335
    2021-03-24 13:20:52 INFO juju-log Closed port 33332 in favor of port 33335
    2021-03-24 13:20:52 INFO juju-log Closed port 33334 in favor of port 33335

Signed-off-by: Mauricio Faria de Oliveira <mfo@canonical.com>
Closes-bug: #1921131
Change-Id: I5ac4b66137faffee82ae0f1e13718f21274f1f56

ceph-radosgw/hooks/hooks.py
@@ -32,6 +32,8 @@ from charmhelpers.core.hookenv import (
     related_units,
     config,
     open_port,
+    opened_ports,
+    close_port,
     relation_set,
     log,
     DEBUG,
@@ -247,7 +249,14 @@ def config_changed():
 
         update_nrpe_config()
 
-        open_port(port=listen_port())
+        port = listen_port()
+        open_port(port)
+        for opened_port in opened_ports():
+            opened_port_number = opened_port.split('/')[0]
+            if opened_port_number != port:
+                close_port(opened_port_number)
+                log('Closed port %s in favor of port %s' %
+                    (opened_port_number, port))
     _config_changed()
 
 

ceph-radosgw/unit_tests/test_hooks.py
@@ -40,6 +40,7 @@ TO_PATCH = [
     'listen_port',
     'log',
     'open_port',
+    'opened_ports',
     'os',
     'relation_ids',
     'relation_set',

commit 6a3424f47c6febf1d35a15bded9bd519897e4ca8

Ensure identical types for comparing port number/string
Fix an oversight and regression in commit 798ca49b2f83
("Close previously opened ports on port config change").

The comparison between an integer and a string (returned
by .split()) is always different and thus when upgrading
the charm 'port 80' is closed.

Make sure the types are set to str. Right now it should
only be needed for port and not opened_port_number; but
let's future proof both sides of the comparison.

(Update: using str() vs int() as apparently int() might
fail but str() should always work no matter what it got;
thanks, Alex Kavanagh!)

Before:

    $ juju run --unit ceph-radosgw/0 opened-ports
    80/tcp

    $ juju upgrade-charm --path . ceph-radosgw

    $ juju run --unit ceph-radosgw/0 opened-ports
    $

    @ log:
    2021-04-05 15:08:04 INFO juju-log Closed port 80 in favor of port 80

    $ python3 -q
    >>> x=80
    >>> y='80/tcp'
    >>> z=y.split('/')[0]
    >>> z
    '80'
    >>> x
    80
    >>> x != z
    True
    >>> x=str(x)
    >>> x != z
    False

After:

    $ juju run --unit ceph-radosgw/1 opened-ports
    80/tcp

    $ juju upgrade-charm --path . ceph-radosgw

    $ juju run --unit ceph-radosgw/1 opened-ports
    80/tcp

Signed-off-by: Mauricio Faria de Oliveira <mfo@canonical.com>
Change-Id: I2bcdfec1459ea45d8f57b850b7fd935c360cc7c1

ceph-radosgw/hooks/hooks.py
@@ -253,7 +253,7 @@ def config_changed():
         open_port(port)
         for opened_port in opened_ports():
             opened_port_number = opened_port.split('/')[0]
-            if opened_port_number != port:
+            if str(opened_port_number) != str(port):
                 close_port(opened_port_number)
                 log('Closed port %s in favor of port %s' %
                     (opened_port_number, port))