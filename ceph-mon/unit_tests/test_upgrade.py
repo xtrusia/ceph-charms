5bc76a462f56be6975a185725595d3975c3c71f3
Luciano Lo Giudice <luciano.logiudice@canonical.com>
2023-07-05 16:23:31 -0300

    Fix ceph-mon upgrade path
    This PR makes some small changes in the upgrade path logic by
    providing a fallback method of fetching the current ceph-mon
    version and adding additional checks to see if the upgrade can
    be done in a sane way.
    
    Closes-Bug: #2024253
    Change-Id: I1ca4316aaf4f0b855a12aa582a8188c88e926fa6

--- ceph-mon/unit_tests/test_upgrade.py
+++ ceph-mon/unit_tests/test_upgrade.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch
 from ceph_hooks import check_for_upgrade
 from test_utils import CharmTestCase
+from charms_ceph.utils import resolve_ceph_version as resolve_ceph_version_orig
 
 
 __author__ = 'Chris Holcombe <chris.holcombe@canonical.com>'
@@ -76,3 +77,51 @@ class UpgradeRollingTestCase(CharmTestCase):
         check_for_upgrade()
         roll_monitor_cluster.assert_not_called()
         add_source.assert_called_with('cloud:bionic-stein', 'some-key')
+
+    @patch('ceph_hooks.ceph.resolve_ceph_version')
+    @patch('ceph_hooks.subprocess.check_output')
+    @patch('ceph_hooks.add_source')
+    @patch('ceph_hooks.ceph.is_bootstrapped')
+    @patch('ceph_hooks.hookenv')
+    @patch('ceph_hooks.ceph.roll_monitor_cluster')
+    def test_check_for_upgrade_no_current_version(self, roll_monitor_cluster,
+                                                  hookenv, is_bootstrapped,
+                                                  add_source, check_output,
+                                                  resolve_ceph_version):
+        _resolve_first = True
+
+        def _resolve_version(arg):
+            nonlocal _resolve_first
+            if _resolve_first:
+                _resolve_first = False
+                return None
+            return resolve_ceph_version_orig(arg)
+
+        resolve_ceph_version.side_effect = _resolve_version
+        check_output.return_value = b"""
+ceph version 16.2.13 (123) pacific (stable)"""
+        is_bootstrapped.return_value = True
+        hookenv.config.side_effect = self.test_config
+        self.test_config.set('source', 'cloud:focal-yoga')
+        check_for_upgrade()
+        roll_monitor_cluster.assert_called()
+        add_source.assert_not_called()
+
+    @patch('ceph_hooks.ceph.resolve_ceph_version')
+    @patch('ceph_hooks.subprocess.check_output')
+    @patch('ceph_hooks.add_source')
+    @patch('ceph_hooks.ceph.is_bootstrapped')
+    @patch('ceph_hooks.hookenv')
+    @patch('ceph_hooks.ceph.roll_monitor_cluster')
+    def test_check_for_upgrade_no_versions(self, roll_monitor_cluster,
+                                           hookenv, is_bootstrapped,
+                                           add_source, check_output,
+                                           resolve_ceph_version):
+        resolve_ceph_version.return_value = None
+        check_output.return_value = b"""
+ceph version 17.2.5 (456) quincy (stable)"""
+        is_bootstrapped.return_value = True
+        hookenv.config.side_effect = self.test_config
+        check_for_upgrade()
+        roll_monitor_cluster.assert_not_called()
+        add_source.assert_not_called()
