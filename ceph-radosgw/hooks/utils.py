bcd5d61be27a401c9df04fc482b18eb766e211d1
Peter Sabaini <peter.sabaini@canonical.com>
2025-01-23 12:34:26 +0100

    Tests: pin boto3
    For https://github.com/boto/boto3/issues/4398
    
    Also lint updates
    
    Signed-off-by: Peter Sabaini <peter.sabaini@canonical.com>
    Change-Id: Ieb248434b482f513060ca8b453e2068d51fde9cf
    Cherry picked from 525245830f9bfef332cf3ea2e505ead98cfe6240

--- ceph-radosgw/hooks/utils.py
+++ ceph-radosgw/hooks/utils.py
@@ -256,10 +256,10 @@ def check_optional_config_and_relations(configs):
                                  "resolve."
                     if (len(zonegroups) > 1 and
                             config('zonegroup') not in zonegroups):
-                        return('blocked', status_msg)
+                        return ('blocked', status_msg)
 
                     if len(zones) > 1 and config('zone') not in zones:
-                        return('blocked', status_msg)
+                        return ('blocked', status_msg)
 
                     if not all(master_configured):
                         return ('blocked', "Failure in Multisite migration, "
