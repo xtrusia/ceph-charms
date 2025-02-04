40645dc77623c6fb303d6ae125878c831d49aa87
Peter Sabaini <peter.sabaini@canonical.com>
2025-02-04 21:09:44 +0100

    CI: clear out docker iptables rules (#8)
    Signed-off-by: Peter Sabaini <peter.sabaini@canonical.com>

---
+++ tests/scripts/actionutils.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+function cleaript() {
+    # Docker can inject rules causing firewall conflicts
+    sudo iptables -P FORWARD ACCEPT  || true
+    sudo ip6tables -P FORWARD ACCEPT || true
+    sudo iptables -F FORWARD  || true
+    sudo ip6tables -F FORWARD || true
+
+}
+
+run="${1}"
+shift
+
+$run "$@"
