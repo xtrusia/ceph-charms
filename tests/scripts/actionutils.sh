71668d86acf69ce6c648b1ddb18704698c10bace
Luciano Lo Giudice <luciano.logiudice@canonical.com>
2025-02-12 13:40:55 -0300

    Misc. improvements to the github workflow
    This PR makes it so that:
    - A single failure in a test run doesn't automatically cancel
      all the other test runs.
    - The upload task has been modified so that the name is equal for
      all jobs. This was necessary because otherwise the charms would
      only download their own built files.
    - Before deploying via juju for the functional testing, the images
      for both containers and VM's are cached.

--- tests/scripts/actionutils.sh
+++ tests/scripts/actionutils.sh
@@ -9,6 +9,16 @@ function cleaript() {
 
 }
 
+function cacheimgs() {
+    lxc launch $1 ctemp 2>&1 >/dev/null
+    lxc launch $1 vmtemp --vm -c limits.cpu=2 -c limits.memory=4GiB -d root,size=25GiB 2>&1 >/dev/null
+    lxc stop ctemp
+    lxc delete ctemp
+    sleep 60
+    lxc stop vmtemp
+    lxc delete vmtemp
+}
+
 run="${1}"
 shift
 
